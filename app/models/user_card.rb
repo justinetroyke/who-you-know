43cb1fa34b77d472c2d25d925c5eacae3725b345
hmesander <haleymesander@gmail.com>
2018-09-09 11:21:35 -0600

    :sparkes: Implements returning a hard deck of cards

--- app/models/user_card.rb
+++ app/models/user_card.rb
@@ -9,6 +9,8 @@ class UserCard < ApplicationRecord
       easy_deck
     elsif difficulty == "medium"
       medium_deck
+    elsif difficulty == "hard"
+      hard_deck
     else
       UserCard.where(difficulty: "unsorted").take(30)
     end
@@ -27,6 +29,14 @@ class UserCard < ApplicationRecord
     deck << UserCard.where(difficulty: "easy").take(2)
     deck << UserCard.where(difficulty: "medium").take(8)
     deck << UserCard.where(difficulty: "hard").take(2)
+    deck.flatten.shuffle
+  end
+
+  def self.hard_deck
+    deck = []
+    deck << UserCard.where(difficulty: "easy").take(2)
+    deck << UserCard.where(difficulty: "medium").take(2)
+    deck << UserCard.where(difficulty: "hard").take(8)
     deck.flatten.shuffle
   end
 end
