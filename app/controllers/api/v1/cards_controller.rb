839d9f34d49f1285f10f25d77dbbf8e720648b8d
hmesander <haleymesander@gmail.com>
2018-09-10 12:55:24 -0600

    :sparkles: Implements returning 400 status and message when not enough sorted cards exist to create a deck of a a given difficulty level

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::CardsController < ApplicationController
     deck = Deck.new(params[:difficulty] ||= "unsorted").get_cards
     if deck.empty?
       render status: 400, json: { message: "User has sorted all their cards." }
+    elsif deck.count < 12
+      render status: 400, json: { message: "User must sort more cards." }
     else
       render json: deck
     end
