f9c66ed11eb0c4b6af84dc8b5ab706a4d150a014
hmesander <haleymesander@gmail.com>
2018-09-11 15:48:47 -0600

    :sparkles: Handles edge case when required user params are not sent in request

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::UsersController < ApplicationController
     user = User.new(username: "nil", api_key: user_params[:api_key], id_token: user_params[:id_token])
     if user.save
       render status: 201, json: { message: "User created successfully."}
+    else
+      render status: 400, json: { message: "Missing required parameters."}
     end
   end
 
