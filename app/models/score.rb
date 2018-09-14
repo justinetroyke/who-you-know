73ea724ebaf1313c9dce030f173894a76cfe23cc
hmesander <haleymesander@gmail.com>
2018-09-13 18:50:18 -0600

    :sparkles: Refactors averages method on score model

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,33 +4,15 @@ class Score < ApplicationRecord
   belongs_to :user
 
   def self.averages(user_id)
-    easy = self.where(difficulty: "easy", user_id: user_id)
-    easy_total_num = 0
-    easy_total_correct = 0
-    easy.each do |score|
-      easy_total_num += score.num_played
-      easy_total_correct += score.num_correct
-    end
-    easy_average = ((easy_total_correct.to_f/easy_total_num) * 100).round(1)
+    levels = ["easy", "medium", "hard"]
+    averages = levels.map { |level| self.calculate_average(level, user_id) }
+    { easy: "#{averages[0]}%", medium: "#{averages[1]}%", hard: "#{averages[2]}%"}
+  end
 
-    medium = self.where(difficulty: "medium", user_id: user_id)
-    medium_total_num = 0
-    medium_total_correct = 0
-    medium.each do |score|
-      medium_total_num += score.num_played
-      medium_total_correct += score.num_correct
-    end
-    medium_average = ((medium_total_correct.to_f/medium_total_num) * 100).round(1)
-
-    hard = self.where(difficulty: "hard", user_id: user_id)
-    hard_total_num = 0
-    hard_total_correct = 0
-    hard.each do |score|
-      hard_total_num += score.num_played
-      hard_total_correct += score.num_correct
-    end
-    hard_average = ((hard_total_correct.to_f/hard_total_num) * 100).round(1)
-
-    { easy: "#{easy_average}%", medium: "#{medium_average}%", hard: "#{hard_average}%"}
+  def self.calculate_average(level, user_id)
+    scores = self.where(difficulty: level, user_id: user_id)
+    total = scores.sum(&:num_played)
+    correct = scores.sum(&:num_correct)
+    ((correct.to_f/total) * 100).round(1)
   end
 end
