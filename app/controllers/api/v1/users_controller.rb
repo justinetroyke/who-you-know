9ff585464f11c9e2a24ee0d1038fb6efc002692e
hmesander <haleymesander@gmail.com>
2018-09-17 12:33:02 -0600

    Returns user id when a new user is created instead of a message

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(username: "nil", api_key: user_params[:api_key], id_token: user_params[:id_token])
     if user.save
-      render status: 201, json: { user: user, message: "User created successfully."}
+      render status: 201, json: user
     else
       render status: 400, json: { message: "Missing required parameters."}
     end
