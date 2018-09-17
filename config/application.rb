cd592e94a2b97e36466c681f8e9e97ad30834407
hmesander <haleymesander@gmail.com>
2018-09-17 10:49:14 -0600

    Adds initial cors access

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,14 @@ module WhoYouKnowBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.load_defaults 5.1
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: :any
+      end
+    end
   end
 end
