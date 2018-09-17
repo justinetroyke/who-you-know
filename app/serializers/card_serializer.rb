35e280dd566c1caae8536b9cc1429c5d384500b5
hmesander <haleymesander@gmail.com>
2018-09-17 10:52:42 -0600

    Adds id field to card serializer

--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,3 +1,3 @@
 class CardSerializer < ActiveModel::Serializer
-  attributes :first_last_name, :company, :school, :job_title, :image_url, :profile_url
+  attributes :id, :first_last_name, :company, :school, :job_title, :image_url, :profile_url
 end
